2514f66f2aec2e2da2514074b172256c997e1c44
Fabio Pozzi <pozzi.fabio@gmail.com>
2018-01-27 10:20:30 +0100

    introducing termbox handling

--- leggo.go
+++ leggo.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"github.com/nsf/termbox-go"
 	"github.com/ungerik/go-rss"
 )
 
 func main() {
+	term_err := termbox.Init()
+	if term_err != nil {
+		panic(term_err)
+	}
+	defer termbox.Close()
+
 	channel, err := rss.Read("http://www.ansa.it/sito/ansait_rss.xml")
 	if err != nil {
 		fmt.Println(err)
@@ -16,5 +23,16 @@ func main() {
 
 	for _, item := range channel.Item {
 		fmt.Println(item.Title)
+	}
+
+loop:
+	for {
+		switch ev := termbox.PollEvent(); ev.Type {
+		case termbox.EventKey:
+			switch ev.Key {
+			case termbox.KeyEsc:
+				break loop
+			}
+		}
 	}
 }
