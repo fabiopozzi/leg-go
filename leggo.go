9fa950ee9e9ffe098f92752b5be46f636c60bc33
Fabio Pozzi <pozzi.fabio@gmail.com>
2018-01-28 23:20:23 +0100

    WIP: refactoring to use tview as terminal UI.

--- leggo.go
+++ leggo.go
@@ -2,19 +2,17 @@ package main
 
 import (
 	"fmt"
-	"github.com/mmcdole/gofeed"
-	"github.com/nsf/termbox-go"
 	"os"
+	"strconv"
+
+	"github.com/gdamore/tcell"
+	"github.com/mmcdole/gofeed"
+	"github.com/rivo/tview"
 )
 
-type configT struct {
-	startLine int
-	numRows   int
-	headline  string
-}
+type Slide func(nextSlide func()) (title string, content tview.Primitive)
 
-var cfg configT
-
+var app = tview.NewApplication()
 var feed *gofeed.Feed
 var curRow int
 
@@ -28,6 +26,7 @@ func parseRss(url string) *gofeed.Feed {
 	return feed
 }
 
+/*
 func printTitle(title string) {
 	runes := []rune(title)
 	bg := termbox.AttrBold | termbox.ColorRed
@@ -52,12 +51,13 @@ func rowDecrement() {
 }
 
 func showArticle() {
+	// TODO: refactor insieme a printNews per estrarre parte rendering.
 	w, _ := termbox.Size()
 	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
 	item := feed.Items[curRow-cfg.startLine]
 	printTitle(item.Title)
 	bg := termbox.AttrBold
-	fg := termbox.AttrBold | termbox.ColorGreen
+	fg := termbox.AttrBold | termbox.ColorWhite
 	y := cfg.startLine
 	runes := []rune(item.Description)
 	i := 0
@@ -72,68 +72,87 @@ func showArticle() {
 	termbox.Flush()
 }
 
-func printNews() {
-	bg := termbox.ColorDefault
-	y := cfg.startLine
-	// TODO: use method to generate output string from item.Title
-	for _, item := range feed.Items {
-		runes := []rune(item.Title)
-		for i := 0; i < len(runes); i++ {
-			if y == curRow {
-				bg = termbox.ColorRed | termbox.AttrBold
-			} else {
-				bg = termbox.AttrBold
-			}
-			fg := termbox.AttrBold | termbox.ColorGreen
-			termbox.SetCell(i+1, y, runes[i], fg, bg)
-		}
-		y++
+*/
+
+func RSS(nextSlide func()) (title string, content tview.Primitive) {
+	table := tview.NewTable().
+		SetFixed(1, 1)
+
+	list := tview.NewList()
+
+	showDescription := func() {
+		// TODO: mostrare il testo quando premi enter.
 	}
-}
-
-func drawAll() {
-	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
-
-	printTitle(cfg.headline)
-	printNews()
-
-	termbox.Flush()
-}
-
-func main() {
-	err := termbox.Init()
-	if err != nil {
-		panic(err)
-	}
-	defer termbox.Close()
 
 	feed = parseRss("http://www.ansa.it/sito/ansait_rss.xml")
 
-	// init cfg
-	cfg.headline = "Titoli:"
-	cfg.startLine = 2
-	cfg.numRows = len(feed.Items) + cfg.startLine
+	list.ShowSecondaryText(false)
 
-	curRow = cfg.startLine
-	drawAll()
-loop:
-	for {
-		switch ev := termbox.PollEvent(); ev.Type {
-		case termbox.EventKey:
-			switch ev.Key {
-			case termbox.KeyEsc:
-				break loop
-			case termbox.KeyArrowDown:
-				rowIncrement()
-			case termbox.KeyArrowUp:
-				rowDecrement()
-			case termbox.KeyArrowRight:
-				showArticle()
-			case termbox.KeyArrowLeft:
-				drawAll()
-			}
-		case termbox.EventResize:
-			drawAll()
+	// add a list element for each feed title
+	shortcut := 'a'
+	for _, item := range feed.Items {
+		list.AddItem(item.Title, "", shortcut, showDescription)
+		shortcut++
+	}
+
+	return "RSS", tview.NewFlex().
+		AddItem(tview.NewFlex().
+			SetDirection(tview.FlexRow).
+			AddItem(list, 10, 1, true).
+			AddItem(table, 0, 1, true), 0, 1, true)
+}
+
+func main() {
+	// all application tabs (To be implemented)
+	slides := []Slide{
+		RSS,
+	}
+
+	// The bottom row has some info on where we are.
+	info := tview.NewTextView().
+		SetDynamicColors(true).
+		SetRegions(true).
+		SetWrap(false)
+
+	// Create pages for all slides
+	curSlide := 0
+	info.Highlight(strconv.Itoa(curSlide))
+	pages := tview.NewPages()
+	previousSlide := func() {
+		curSlide = (curSlide - 1 + len(slides)) % len(slides)
+		info.Highlight(strconv.Itoa(curSlide))
+		pages.SwitchToPage(strconv.Itoa(curSlide))
+	}
+	nextSlide := func() {
+		curSlide = (curSlide + 1) % len(slides)
+		info.Highlight(strconv.Itoa(curSlide))
+		pages.SwitchToPage(strconv.Itoa(curSlide))
+	}
+
+	for index, slide := range slides {
+		title, primitive := slide(nextSlide)
+		pages.AddPage(strconv.Itoa(index), primitive, true, index == curSlide)
+		fmt.Fprintf(info, `%d ["%d"][darkcyan]%s[white][""]  `, index+1, index, title)
+	}
+
+	layout := tview.NewFlex().
+		SetDirection(tview.FlexRow).
+		AddItem(pages, 0, 1, true).
+		AddItem(info, 1, 1, false)
+
+	// keybindings to move between slides and exit.
+	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Key() == tcell.KeyCtrlN {
+			nextSlide()
+		} else if event.Key() == tcell.KeyCtrlP {
+			previousSlide()
+		} else if event.Key() == tcell.KeyEsc {
+			app.Stop()
 		}
+		return event
+	})
+
+	if err := app.SetRoot(layout, true).SetFocus(layout).Run(); err != nil {
+		panic(err)
 	}
 }
